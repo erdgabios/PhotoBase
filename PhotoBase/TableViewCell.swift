121336b8997cea346ae3218402786a03f721e4ea
Gabriel Erdelyi <erdgabios@gmail.com>
2017-01-08 17:34:30 +0100

    final

---
+++ PhotoBase/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  PhotoBase
+//
+//  Created by G on 08/01/2017.
+//  Copyright © 2017 erdgabios. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
